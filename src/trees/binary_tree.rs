2658ada81fb6ffbc9f624a0742b8b92b56eaa80d
bagohart <bagohart@gmx.de>
2020-07-14 18:18:43 +0200

    added into_iter the canonical 4 variants

--- src/trees/binary_tree.rs
+++ src/trees/binary_tree.rs
@@ -1,8 +1,10 @@
+use std::collections::VecDeque;
 use std::fmt;
 use std::fmt::Display;
 
 // todo:
-// inorder, preorder, postorder
+// inorder, preorder, postorder, levelorder
+// iter,iter_mut,into_iter
 
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -82,35 +84,100 @@ pub struct IntoIterInOrder<T> {
     stack: Vec<Box<Node<T>>>,
 }
 
+pub struct IntoIterPreOrder<T> {
+    stack: Vec<Box<Node<T>>>,
+}
+
+pub struct IntoIterPostOrder<T> {
+    stack: Vec<Box<Node<T>>>,
+}
+
+pub struct IntoIterLevelOrder<T> {
+    queue: VecDeque<Box<Node<T>>>,
+}
+
 impl<T> BinaryTree<T> {
-    pub fn into_iter(self) -> IntoIterInOrder<T> {
+    pub fn into_iter_in_order(self) -> IntoIterInOrder<T> {
         IntoIterInOrder {
             stack: self.root.into_iter().collect(),
+        }
+    }
+    pub fn into_iter_pre_order(self) -> IntoIterPreOrder<T> {
+        IntoIterPreOrder {
+            stack: self.root.into_iter().collect(),
+        }
+    }
+    pub fn into_iter_post_order(self) -> IntoIterPostOrder<T> {
+        IntoIterPostOrder {
+            stack: self.root.into_iter().collect(),
+        }
+    }
+    pub fn into_iter_level_order(self) -> IntoIterLevelOrder<T> {
+        IntoIterLevelOrder {
+            queue: self.root.into_iter().collect(),
         }
     }
 }
 
 impl<T> Iterator for IntoIterInOrder<T> {
     type Item = T;
-    // let last: Box<Node<T>> = self.stack.pop()?;
-    // while last.left.is_some() {
-    //     let left = last.left.take().unwrap();
-    //     let
-    // implementing this with mutable borrows on the stack doesn not work
-    // because then there are two mutable borrows to the stack
-    // or does it?
     fn next(&mut self) -> Option<Self::Item> {
-        // this is actually a box, but there is some casting magic here ?_?
-        let last: &mut Node<T> = self.stack.last_mut()?;
+        let mut last: &mut Node<T> = self.stack.last_mut()?; // box to mut implicit?
         while last.left.is_some() {
-            let left = last.left.take().unwrap();
+            let left = last.left.take().unwrap(); // cannot inline (borrowing rules!)
             self.stack.push(left);
             last = self.stack.last_mut().unwrap();
         }
-        let last: Box<Node<T>> = self.stack.pop().unwrap();
+        let mut last: Box<Node<T>> = self.stack.pop().unwrap();
         let right: Option<Box<Node<T>>> = last.right.take();
         right.map(|right| self.stack.push(right));
         Some(last.elem)
+    }
+}
+
+impl<T> Iterator for IntoIterPreOrder<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut last: Box<Node<T>> = self.stack.pop()?;
+        let left = last.left.take();
+        let right = last.right.take();
+        right.map(|right| self.stack.push(right));
+        left.map(|left| self.stack.push(left));
+        Some(last.elem)
+    }
+}
+
+impl<T> Iterator for IntoIterPostOrder<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut last: &mut Node<T> = self.stack.last_mut()?;
+        while last.left.is_some() || last.right.is_some() {
+            if last.left.is_some() {
+                let left: Box<Node<T>> = last.left.take().unwrap();
+                self.stack.push(left);
+                last = self.stack.last_mut().unwrap();
+            } else
+            /* if last.right.is_some() */
+            {
+                let right: Box<Node<T>> = last.right.take().unwrap();
+                self.stack.push(right);
+                last = self.stack.last_mut().unwrap();
+            }
+        }
+        let last: Box<Node<T>> = self.stack.pop().unwrap();
+        Some(last.elem)
+    }
+}
+
+impl<T> Iterator for IntoIterLevelOrder<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut first: Box<Node<T>> = self.queue.pop_front()?;
+        let left = first.left.take();
+        let right = first.right.take();
+        left.map(|left| self.queue.push_back(left));
+        right.map(|right| self.queue.push_back(right));
+        Some(first.elem)
     }
 }
 
@@ -164,6 +231,60 @@ impl<T: Display> Display for BinaryTree<T> {
 mod tests {
     use super::BinaryTree;
     use super::Node;
+
+    fn create_tree() -> BinaryTree<i32> {
+        let mut root = Node::new(1);
+        root.set_left_child(2);
+        root.set_right_child(3);
+        root.right.as_mut().unwrap().set_right_child(6);
+        root.left.as_mut().unwrap().set_left_child(4);
+        root.left.as_mut().unwrap().set_right_child(5);
+        root.left
+            .as_mut()
+            .unwrap()
+            .left
+            .as_mut()
+            .unwrap()
+            .set_left_child(7);
+        root.left
+            .as_mut()
+            .unwrap()
+            .right
+            .as_mut()
+            .unwrap()
+            .set_left_child(8);
+        root.left
+            .as_mut()
+            .unwrap()
+            .right
+            .as_mut()
+            .unwrap()
+            .set_right_child(9);
+        BinaryTree::new(root)
+        //         1
+        //        /   \
+        //       2     3
+        //     /   \     \
+        //    4     5     6
+        //   /     / \
+        //  7     8   9
+    }
+
+    #[test]
+    fn iter() {
+        let tree = create_tree();
+        let inorder: Vec<i32> = tree.into_iter_in_order().collect();
+        assert_eq!(inorder, vec![7, 4, 2, 8, 5, 9, 1, 3, 6]);
+        let tree = create_tree();
+        let preorder: Vec<i32> = tree.into_iter_pre_order().collect();
+        assert_eq!(preorder, vec![1, 2, 4, 7, 5, 8, 9, 3, 6]);
+        let tree = create_tree();
+        let postorder: Vec<i32> = tree.into_iter_post_order().collect();
+        assert_eq!(postorder, vec![7, 4, 8, 9, 5, 2, 6, 3, 1]);
+        let tree = create_tree();
+        let levelorder: Vec<i32> = tree.into_iter_level_order().collect();
+        assert_eq!(levelorder, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
 
     #[test]
     fn basics() {
